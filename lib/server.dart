67dc503e75c59c8b915b8d551bc0fd662ebec9e8
BOSSkabab <amit.askof@gmail.com>
2025-05-13 18:19:35 +0300

    created is server running function

--- lib/server.dart
+++ lib/server.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void runServer() async {
-  var handler = const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+  var handler = const Pipeline()
+      .addMiddleware(logRequests())
+      .addHandler(_echoRequest);
 
   var server = await shelf_io.serve(handler, 'localhost', 8080);
 
@@ -12,4 +15,16 @@ void runServer() async {
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-Response _echoRequest(Request request) => Response.ok('Request for "${request.url}"');
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
+
+bool isServerRunning() {
+  try {
+    ServerSocket.bind('localhost', 8080).then((socket) {
+      socket.close();
+    });
+    return false;
+  } catch (_) {
+    return true;
+  }
+}
