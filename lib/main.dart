2ce00f551a76f0b357f945c0fead17986cea8e51
BOSSkabab <amit.askof@gmail.com>
2025-05-13 18:30:43 +0300

    added start server button no working

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+import 'package:dart_server/server.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool isRunning = false;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          // child: TextButton(onPressed: , child: child),
+          child: TextButton(
+            style: TextButton.styleFrom(backgroundColor: isRunning ? Colors.red : Colors.green),
+            onPressed: () async {
+              isRunning = (await http.get(Uri.parse('http://localhost:8080'))).statusCode == 200;
+              isRunning ? http.get(Uri.parse('http://localhost:8080/close')) : runServer();
+            },
+            child: Text(isRunning ? 'Close Server' : 'Run Server'),
+          ),
         ),
       ),
     );
--- lib/server.dart
+++ lib/server.dart
@@ -1,30 +1,17 @@
-import 'dart:io';
+// import 'dart:developer';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void runServer() async {
-  var handler = const Pipeline()
-      .addMiddleware(logRequests())
-      .addHandler(_echoRequest);
+  var handler = const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
 
-  var server = await shelf_io.serve(handler, 'localhost', 8080);
-
-  // Enable content compression
-  server.autoCompress = true;
+  var server = await shelf_io.serve(handler, 'localhost', 11690);
 
   print('Serving at http://${server.address.host}:${server.port}');
 }
 
-Response _echoRequest(Request request) =>
-    Response.ok('Request for "${request.url}"');
-
-bool isServerRunning() {
-  try {
-    ServerSocket.bind('localhost', 8080).then((socket) {
-      socket.close();
-    });
-    return false;
-  } catch (_) {
-    return true;
-  }
+Response _echoRequest(Request request) {
+  // log(request.url.path);
+  // switch (request.url.path) {}
+  return Response.ok('Request for "${request.url}"');
 }
